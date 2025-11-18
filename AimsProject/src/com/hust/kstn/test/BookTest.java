082b5164f27aad1e7dc2179f763388cc6d6cb332
Linh Chu Van <linh.chuvan.777@gmail.com>
2025-11-18 16:29:57 +0700

    Book and BookTest

---
+++ AimsProject/src/com/hust/kstn/test/BookTest.java
@@ -0,0 +1,22 @@
+package com.hust.kstn.test;
+
+import com.hust.kstn.models.BookAuthor;
+import com.hust.kstn.models.Book;
+
+public class BookTest {
+    public static void main(String[] args) {
+        BookAuthor author1 = new BookAuthor("Nguyen Nhat Anh", 1960, "Tac gia sach bup sen xanh");
+        BookAuthor author2 = new BookAuthor("Nguyen Cong Son", 1970, "Tac gia sach nghin le mot dem");
+        BookAuthor author3 = new BookAuthor("Nguyen Dang Khoa", 1980, "Tac gia bach tuyet va 7 chu lun");
+
+        BookAuthor[] authors1 = {author1, author2};
+        Book book1 = new Book("Giai tich 1", "giao duc", 149000.0, 10000, authors1);
+
+        System.out.println(book1);
+
+        BookAuthor[] authors2 = {author3};
+        Book book2 = new Book("Thanh Giong", "Giao duc", 49000, 1000, authors2);
+
+        System.out.println(book2);
+    }
+}
